a142ff04890313eeda700d779c529e59aa6ed4c9
GNC <145757613+GMF21@users.noreply.github.com>
2024-12-05 18:00:37 -0100

    Create 10x10.cpp

---
+++ 10x10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    // número inicial
+    int numero = 1;
+
+    // tamanho do quadrado 
+    const int tamanho = 10; //para nao ser alterado
+
+    // ciclo for pah fazer as linhas
+    for (int i = 0; i < tamanho; ++i) {
+        // aqui pahs colunas
+        for (int x = 0; x < tamanho; ++x) {
+            
+            cout << setw(4) << numero++;
+        }
+
+        cout << endl;
+        
+        
+    }
+
+    return 0;
+}
