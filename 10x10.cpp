c090d3932345ff23da41eb566528c85e1dd736b0
GNC <145757613+GMF21@users.noreply.github.com>
2024-12-05 22:20:48 -0100

    Update 10x10.cpp

--- 10x10.cpp
+++ 10x10.cpp
@@ -11,9 +11,9 @@ int main() {
     const int tamanho = 10; //para nao ser alterado
 
     // ciclo for pah fazer as linhas
-    for (int i = 0; i < tamanho; ++i) {
+    for (int i = 0; i < tamanho; i++) {
         // aqui pahs colunas
-        for (int x = 0; x < tamanho; ++x) {
+        for (int x = 0; x < tamanho; x++) {
             
             cout << setw(4) << numero++; // o numero que for exibido  nesse caso com 4 caracteres
         }
